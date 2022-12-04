9600898465a58f114ebe7dc7642dca0051cb716a
nope0001 <maier94t@gmail.com>
2022-12-04 16:11:57 +0100

    day3

--- day3.py
+++ day3.py
@@ -1,26 +1,63 @@
-"""--- Day 3: Rucksack Reorganization ---"""
+import string
 
 
 def get_data(input_data):
     with open(input_data, "r", encoding="utf8") as data:
-        return data.readlines()
+        return data.read().split("\n")
 
 
-def part1(data):
-    rucksack = []
-    for line in data:
-        list1 = []
-        for i in range(0, len(line), len(line)//2 ):
-            list1.append(line[i:i+len(line)//2])
-            if len(list1) == 3:
-                del list1[2]
-        rucksack.append(list1)
-    print(rucksack)
+def part1():
+    alphabet = list(string.ascii_lowercase)
+
+    upperAlphabet = list(string.ascii_uppercase)
+
+    rucksackSum = 0
+
+    for items in get_data(".\\input_data\\3.txt"):
+        if items != "":
+            firstContainer = slice(0, len(items) // 2)
+            secondContainer = slice(len(items) // 2, len(items))
+            for letter in list(items[firstContainer]):
+                if letter in items[secondContainer]:
+                    if letter not in alphabet:
+                        rucksackSum += upperAlphabet.index(letter) + 27
+                        break
+                    else:
+                        rucksackSum += alphabet.index(letter) + 1
+                        break
+
+    return rucksackSum
 
 
-def part2(data):
-    print("todo")
+def part2():
+    alphabet = list(string.ascii_lowercase)
+
+    upperAlphabet = list(string.ascii_uppercase)
+    rucksackSum = 0
+
+    line = 0
+
+    itemList = []
+
+    for items in get_data(".\\input_data\\3.txt"):
+        if items != "" and line == 2:
+            line = 0
+            itemList.append(items)
+            for letter in itemList[0]:
+                if letter in itemList[1] and letter in itemList[2]:
+                    if letter not in alphabet:
+                        rucksackSum += upperAlphabet.index(letter) + 27
+                        break
+                    else:
+                        rucksackSum += alphabet.index(letter) + 1
+                        break
+            itemList = []
+        else:
+            itemList.append(items)
+            line += 1
+
+    return rucksackSum
 
 
-print(part1(get_data(".\\input_data\\3.txt")))
-# print(part2(get_data(".\\input_data\\3.txt")))
+print(part1())
+print(part2())
